36dc545803cd43839638d2c9c685b99d0a9a02ef
alekzandev <aleksandev@proton.me>
2024-11-26 23:18:17 -0500

    Catch up w1 class

---
+++ modules/1/print-data-structure/src/main.rs
@@ -0,0 +1,50 @@
+fn main() {
+    println!("Common Rust Collections");
+
+    // Sequences
+    println!("\n\tSequences");
+    println!("\t\tVectors: https://doc.rust-lang.org/std/vec/struct.Vec.html");
+    println!("\t\tArrays: https://doc.rust-lang.org/std/primitive.array.html");
+    println!("\t\tSlices: https://doc.rust-lang.org/std/primitive.slice.html");
+    println!("\t\tStrings: https://doc.rust-lang.org/std/primitive.str.html");
+
+    // Maps
+    println!("\n\tMaps");
+    println!("\t\tHashMap: https://doc.rust-lang.org/std/collections/struct.HashMap.html");
+    println!("\t\tBTreeMap: https://doc.rust-lang.org/std/collections/struct.BTreeMap.html");
+
+    // Sets
+    println!("\n\tSets");
+    println!("\t\tHashSet: https://doc.rust-lang.org/std/collections/struct.HashSet.html");
+    println!("\t\tBTreeSet: https://doc.rust-lang.org/std/collections/struct.BTreeSet.html");
+
+    // Tuples
+    println!("\n\tTuples");
+    println!("\t\tTuples: https://doc.rust-lang.org/std/primitive.tuple.html");
+
+    // Option
+    println!("\n\tOption");
+    println!("\t\tOption: https://doc.rust-lang.org/std/option/enum.Option.html");
+
+    // Result
+    println!("\n\tResult");
+    println!("\t\tResult: https://doc.rust-lang.org/std/result/enum.Result.html");  
+
+    // Iterator
+    println!("\n\tIterator");
+    println!("\t\tIterator: https://doc.rust-lang.org/std/iter/trait.Iterator.html");
+
+    // Closure
+    println!("\n\tClosure");
+    println!("\t\tClosure: https://doc.rust-lang.org/std/ops/trait.Fn.html");
+
+    // Pattern Matching
+    println!("\n\tPattern Matching");
+    println!("\t\tPattern Matching: https://doc.rust-lang.org/book/ch18-03-pattern-syntax.html");
+
+    // Match
+    println!("\n\tMatch");
+    println!("\t\tMatch: https://doc.rust-lang.org/std/keyword.match.html");
+
+
+}
